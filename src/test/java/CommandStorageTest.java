51731ff77f95a82af6ee5e28f7d58219374023db
ehc35 <ehc35@drexel.edu>
2021-11-08 12:09:00 -0500

    Added a few more test cases

--- src/test/java/CommandStorageTest.java
+++ src/test/java/CommandStorageTest.java
@@ -15,6 +15,7 @@ public class CommandStorageTest {
     void add_one_invalid_command() {
         commandStorage.addInvalidCommand("crete checking 12345678 4.0");
         assertEquals("crete checking 12345678 4.0", commandStorage.getInvalidCommands().get(0));
+        assertEquals(1, commandStorage.getInvalidCommands().size());
     }
 
     @Test
@@ -23,5 +24,12 @@ public class CommandStorageTest {
         commandStorage.addInvalidCommand("depost 12345678 800");
         assertEquals("create cheking 12345678 4.0", commandStorage.getInvalidCommands().get(0));
         assertEquals("depost 12345678 800", commandStorage.getInvalidCommands().get(1));
+    }
+
+    @Test
+    void check_invalid_commands_size_is_two() {
+        commandStorage.addInvalidCommand("create cheking 12345678 4.0");
+        commandStorage.addInvalidCommand("depost 12345678 800");
+        assertEquals(2, commandStorage.getInvalidCommands().size());
     }
 }
